55204ce6ed36eef1fb309e11b6a77b6a85338130
arminbkh0921@yahoo.com <arminbkh0921@yahoo.com>
2022-02-25 14:21:06 +0330

    create Navbar & Layout components

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "styles/Navbar.module.css";
+
+const links = [
+  { id: 1, title: "Home", href: "/" },
+  { id: 2, title: "Blogs", href: "/blogs" },
+  { id: 3, title: "Dashboard", href: "/dashboard" },
+  { id: 4, title: "login", href: "#" },
+  { id: 5, title: "signup", href: "#" },
+];
+
+const Navbar = () => {
+  const router = useRouter();
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.logo}>NEXT AUTH</h1>
+      <nav>
+        <ul className={styles.navbarNav}>
+          {links.map((link) => (
+            <li className={styles.navItem} key={link.id}>
+              <Link href={link.href}>
+                <a
+                  className={`${styles.navLink} ${
+                    router.pathname === link.href && styles.activeLink
+                  }`}
+                >
+                  {link.title}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
