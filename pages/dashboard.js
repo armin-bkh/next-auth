d4f28aedc9e6c37e54b909080211c6edd8d35dc5
arminbkh0921@yahoo.com <arminbkh0921@yahoo.com>
2022-02-27 00:49:42 +0330

    securing

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,30 +1,22 @@
-import { useEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { getSession, signIn, useSession } from "next-auth/react";
 const DashboardPage = () => {
   // const [loading, setLoading] = useState(true);
   // const [userData, setUserData] = useState(null);
   const { data: session, status } = useSession();
 
-  // useEffect(() => {
-  //   (async function () {
-  //     const session = await getSession();
-  //     if (!session) signIn();
-  //     else {
-  //       setLoading(false);
-  //       console.log(session.user);
-  //       setUserData(session.user);
-  //     }
-  //   })();
-  // }, []);
+  useLayoutEffect(() => {
+    if (status === "unauthenticated") signIn();
+  }, [status]);
 
-  if(status === 'loading') {
-    return <h1>Loading...</h1>
+  if (status === "loading") {
+    return <h1>Loading...</h1>;
   }
 
   return (
     <>
       <h1>Dashboard page</h1>
-      <p>{session.user.name}</p>
+      <p>{session?.user?.name}</p>
     </>
   );
 };
