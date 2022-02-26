fb40b017ebe02b0e1819206917408d85c34a39f0
arminbkh0921@yahoo.com <arminbkh0921@yahoo.com>
2022-02-27 00:18:04 +0330

    handle securing dashboard page with getSession function

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import { getSession, signIn } from "next-auth/react";
 const DashboardPage = () => {
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    (async function () {
+      const session = await getSession();
+      if (!session) signIn();
+      else {
+        setLoading(false);
+        console.log(session.user);
+        setUserData(session.user);
+      }
+    })();
+  }, []);
+
+  if(loading && !userData) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <>
       <h1>Dashboard page</h1>
+      <p>{userData?.name}</p>
     </>
   );
 };
