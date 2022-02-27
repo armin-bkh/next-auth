7e50bf3b1fa57690de742130bf6c055130023582
arminbkh0921@yahoo.com <arminbkh0921@yahoo.com>
2022-02-27 09:30:30 +0330

    handle securing blog page server-sider

--- pages/blogs.js
+++ pages/blogs.js
@@ -2,6 +2,14 @@ import { getSession, useSession } from "next-auth/react";
 
 export const getServerSideProps = async (context) => {
   const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/api/auth/signin?callbackUrl=http://localhost:3000/blogs",
+        permanent: false,
+      },
+    };
+  }
   return {
     props: {
       session,
